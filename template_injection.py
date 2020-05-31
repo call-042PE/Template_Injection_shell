ad0c6d7a7893d35aa3ac495956dae4228ba39b93
Mr. Anderson <44845040+call-042PE@users.noreply.github.com>
2020-05-31 20:49:00 +0200

    Update template_injection.py

--- template_injection.py
+++ template_injection.py
@@ -8,7 +8,6 @@ def sendcommand(url,method,param):
 
 def main():
 	try:
-			#and sys.argv[2] == "--method" or "-m" and sys.argv[3] == "--cmd" or "-c"
 		sendcommand(sys.argv[1],sys.argv[2],sys.argv[3])
 	except IndexError:
 		print("url: url of the webapp to exploit")
